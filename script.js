0e479f334d544f7927ac24ff511a1a49d4b0964f
Tarun0204 <127747782+Tarun0204@users.noreply.github.com>
2024-09-07 15:58:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var icon = document.getElementById("moonIcon");
 
-icon.onclick = function() {
-    document.body.classList.toggle("dark-theme")
-    if(document.body.classList.contains("dark-theme")) {
-        icon.src = "images/sun.jpg";
-    } else {
-        icon.src = "images/moon.jpg"
-    }
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+    icon.src = "images/sun.jpg"; 
+} else {
+    icon.src = "images/moon.jpg"; 
 }
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.jpg";
+        localStorage.setItem("theme", "dark"); 
+    } else {
+        icon.src = "images/moon.jpg";
+        localStorage.setItem("theme", "light"); 
+    }
+};
