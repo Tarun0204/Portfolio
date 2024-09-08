8c853b1d4eb2202547bff819fb2576e0318278df
Tarun0204 <127747782+Tarun0204@users.noreply.github.com>
2024-09-08 19:34:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Navbar 
-function myMenuFunction() {
-    var menuBtn = document.getElementById("myNavMenu");
-
-    if (menuBtn.className === "nav-menu") {
-        menuBtn.className += " responsive";
-    } else {
-        menuBtn.className = "nav-menu";
-    }
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
 }
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}    
 
 //Dark Mode
 var icon = document.getElementById("moonIcon");
